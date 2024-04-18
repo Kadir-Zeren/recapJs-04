0d211f7ad837901d16edc282f3977e146fe0b418
kadir-zeren <k4dzeren@gmail.com>
2024-04-18 12:46:34 +0300

    for

--- for.js
+++ for.js
@@ -1,3 +1,19 @@
-for (let i = 1; i <= 128; i++) {
-  console.log(`${i}-2024`);
-}
+// for (let i = 1; i <= 128; i++) {
+//   console.log(`${i}-2024`);
+// }
+
+// const n = prompt("Enter your number");
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+//   console.log(sum);
+// }
+// console.log(`SUM: ${sum}`);
+
+// const adet = prompt("How many numbers:?");
+
+// for (let sayac = 1; sayac <= adet; sayac++) {
+//   const rasgeleSayi = Math.round(Math.random() * 100);
+//   console.log(`${sayac}. sayiniz : ${rasgeleSayi}`);
+// }
